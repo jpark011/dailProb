3f06ed477fc1fbdf8af2f960857cc2304435f24e
Jay Park <wngt3319@gmail.com>
2019-01-15 20:21:13 -0500

    Day 41

---
+++ 41flightTicket.js
@@ -0,0 +1,26 @@
+// Given an unordered list of flights taken by someone, each represented as (origin, destination) pairs, and a starting airport, compute the person's itinerary. If no such itinerary exists, return null. If there are multiple possible itineraries, return the lexicographically smallest one. All flights must be used in the itinerary.
+
+// For example, given the list of flights [('SFO', 'HKO'), ('YYZ', 'SFO'), ('YUL', 'YYZ'), ('HKO', 'ORD')] and starting airport 'YUL', you should return the list ['YUL', 'YYZ', 'SFO', 'HKO', 'ORD'].
+
+// Given the list of flights [('SFO', 'COM'), ('COM', 'YYZ')] and starting airport 'COM', you should return null.
+
+// Given the list of flights [('A', 'B'), ('A', 'C'), ('B', 'C'), ('C', 'A')] and starting airport 'A', you should return the list ['A', 'B', 'C', 'A', 'C'] even though ['A', 'C', 'A', 'B', 'C'] is also a valid itinerary. However, the first one is lexicographically smaller.
+
+function flightTrip( set, src ) {
+    let nextTrip = null;
+    for ( let ticket of set ) {
+        if ( src === ticket[0] ) {
+            nextTrip = ticket;
+        }
+    }
+    if ( !nextTrip ) return null;
+    
+    set.delete( nextTrip );
+    let result = flightTrip( set, nextTrip[1] );
+
+    if ( !result ) {
+        return null;
+    } else {
+        return src + result;
+    }
+}
